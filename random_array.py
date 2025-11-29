c2c37be2744def1bbb25eacea7610110d7ed22c5
Alisa Umyarova <alisa.umyarova@gmail.com>
2025-11-29 21:41:19 +0700

    random array

--- random_array.py
+++ random_array.py
@@ -1,5 +1,8 @@
-# генератор случайных массивов с параметрами по умолчанию
+# генерация случайных массивов
 import random
 
-def generate_array(size, min_val=0, max_val=50):
-    return [random.randint(min_val, max_val) for _ in range(size)]
+def random_array(size, min_val=0, max_val=50):
+    arr1 = [random.randint(min_val, max_val) for _ in range(size)]
+    arr2 = [random.randint(min_val, max_val) for _ in range(size)]
+    return arr1, arr2
+
